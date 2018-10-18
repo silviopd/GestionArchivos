1fa8d28dfe7affda5e5fb68111fadfac02c35a0c
silviopd <silviopd01@gmail.com>
2018-10-18 17:25:03 -0500

    Initial commit

---
+++ src/gestionarchivos/Conexion.java
@@ -0,0 +1,58 @@
+package gestionarchivos;
+
+import java.sql.*;
+
+public class Conexion {
+
+    private String controlador = "org.postgresql.Driver";
+    private String cadenaConexion = "jdbc:postgresql://localhost:5432/bd_estrella";
+    private String usuario = "postgres";
+    private String clave = "silviopd";
+
+    private Connection conexion;
+
+    protected Connection abrirConexion() throws Exception {
+        Class.forName(this.controlador);
+        conexion = DriverManager.getConnection(
+                cadenaConexion,
+                usuario,
+                clave);
+
+        return conexion;
+    }
+
+    private void cerrarConexion(Connection con) throws Exception {
+        con.close();
+        con = null;
+    }
+
+    protected ResultSet ejecutarSQLSelect(String sql) throws Exception {
+        Statement sentencia = null;
+        ResultSet resultado = null;
+
+        sentencia = this.abrirConexion().createStatement();
+        resultado = sentencia.executeQuery(sql);
+        this.cerrarConexion(conexion);
+
+        return resultado;
+    }
+
+    protected ResultSet ejecutarSQLSelectSP(PreparedStatement sentencia) throws Exception {
+        ResultSet resultado = null;
+        resultado = sentencia.executeQuery();
+        this.cerrarConexion(conexion);
+        return resultado;
+    }
+
+    protected int ejecutarSQLsp(PreparedStatement sentencia, Connection con) throws Exception {
+        int resultado = 0;
+        try {
+            resultado = sentencia.executeUpdate();
+        } catch (Exception e) {
+            con.rollback();
+            throw e;
+        }
+        return resultado;
+    }
+
+}
---
+++ src/gestionarchivos/GestionA.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionarchivos;
+import java.io.*;
+
+/**
+ *
+ * @author Oscar
+ */
+public class GestionA {
+    FileInputStream entrada;
+    FileOutputStream salida;
+    File archivo;
+    
+    public GestionA(){
+        
+    }
+    
+    /*Abrir un archivo de texto*/
+    public String AbrirATexto(File archivo){
+        String contenido="";
+        try {
+            entrada = new FileInputStream(archivo);
+            int ascci;
+            while((ascci = entrada.read())!= -1){
+                char carcater = (char)ascci;
+                contenido += carcater;
+            }
+        } catch (Exception e) {
+        }
+        return contenido;
+    }
+    
+    /*Guardar archivo de texto*/
+    public String GuardarATexto(File archivo, String contenido){
+        String respuesta=null;
+        try {
+            salida = new FileOutputStream(archivo);
+            byte[] bytesTxt = contenido.getBytes();
+            salida.write(bytesTxt);
+            respuesta = "Se guardo con exito el archivo";
+        } catch (Exception e) {
+        }
+        return respuesta;
+    }
+    
+    /*Abrir una imagen*/
+    public byte[] AbrirAImagen(File archivo){
+        byte[] bytesImg = new byte[1024*100];
+        try {
+            entrada = new FileInputStream(archivo);
+            entrada.read(bytesImg);
+        } catch (Exception e) {
+        }
+        return bytesImg;
+    }
+    
+    /*Guardar imagen*/
+    public String GuardarAImagen(File archivo, byte[] bytesImg){
+        String respuesta=null;
+        try {
+            salida = new FileOutputStream(archivo);
+            salida.write(bytesImg);
+            respuesta = "La imagen se guardo con exito.";
+        } catch (Exception e) {
+        }
+        return respuesta;
+    }
+}
